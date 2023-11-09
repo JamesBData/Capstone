7fd5e29431c205a080ee160acc7cc561fb0b7ea7
James Barnes <james.barnes11998@gmail.com>
2023-11-08 20:08:21 -0500

    resolved

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, ROUND_HALF_UP
 class food:
     def __init__(self, name, category, description, price):
         self.name = name
@@ -16,3 +17,10 @@ Drink3 = food("Root Beer", "Drink", "Classic Root Beer Taste", 1.50)
 Combo1 = food("Combo 1", "Combo", "Comes With Beef Burger, Curly Fries, And Coca-Cola", 7.00)
 Combo2 = food("Combo 2", "Combo", "Comes With Chicken Sandwich, Onion Rings, And Pepsi", 7.00)
 Combo3 = food("Combo 3", "Combo", "Comes With Fish Sandwich, Cheese Curds, And Root Beer", 7.00)
+
+
+
+
+
+
+
